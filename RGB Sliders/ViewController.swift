df604b2a45ecebe7d2112111b9eb2e2c1f951c0a
Blake Robinson <blakerobinson@Blakes-MacBook-Pro.local>
2016-11-28 11:53:15 -0500

    added sliders and nav controller

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Aren't these too view related to be in the view controller?
+        colorSquare.layer.borderColor = UIColor.black.cgColor
+        colorSquare.layer.borderWidth = 1
+        updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        
+        let red = redSlider.value
+        let green = greenSlider.value
+        let blue = blueSlider.value
+        
+        colorSquare.backgroundColor = UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
